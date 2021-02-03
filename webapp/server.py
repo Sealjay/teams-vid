8d7103e430ffae53fb0f9c151325b65bd7e558c7
Chris Lloyd-Jones <CortalUX@users.noreply.github.com>
2021-02-03 00:57:43 +0000

    migrate: converted from csharp to python

--- webapp/server.py
+++ webapp/server.py
@@ -1,14 +1,74 @@
 import databases
 import sqlalchemy
+import uuid
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, RedirectResponse
 from starlette.routing import Route
+from starlette.applications import Starlette
+from starlette.routing import Mount
+from starlette.staticfiles import StaticFiles
+from starlette.applications import Starlette
+from starlette.routing import Route, Mount
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+import aiofiles
+import multipart
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-app = Starlette(debug=True, routes=[
-    Route('/', homepage),
-])
+async def gallery(request):
+    return templates.TemplateResponse("gallery.html", {"request": request})
+
+
+async def record(request):
+    return templates.TemplateResponse("record.html", {"request": request})
+
+
+async def play(request):
+    return templates.TemplateResponse("play.html", {"request": request})
+
+
+async def about(request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+async def add_file_to_db(filename, file_contents):
+    extension = filename.split(".")[-1].lower()
+    file_uuid = uuid.uuid4()
+    async with aiofiles.open(f"uploads/{file_uuid}.{extension}", "wb") as file:
+        await file.write(file_contents)
+        await file.close()
+
+
+async def file_upload(request):
+
+    form = await request.form()
+    video_recording = form["video_recording"]
+    filename = video_recording.filename
+    contents = await video_recording.read()
+    await add_file_to_db(filename, contents)
+    return templates.TemplateResponse("uploaded.html", {"request": request})
+
+
+routes = [
+    Route("/", homepage),
+    Route("/gallery", gallery),
+    Route("/file_upload", file_upload, methods=["POST"]),
+    Route("/record", record),
+    Route("/play", play),
+    Route("/about", about),
+    Mount(
+        "/static",
+        app=StaticFiles(directory="static", packages=["bootstrap4"]),
+        name="static",
+    ),
+]
+
+
+app = Starlette(debug=True, routes=routes)
